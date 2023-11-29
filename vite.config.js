23ad8fee2459ec1a7de649525acf5800dcda8a0c
mimu <akito.mimura@nijibox.co.jp>
2023-11-29 16:14:53 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { resolve } from "path";
-// import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   base: "/dist",
   build: {
     outDir: resolve(__dirname, "dist"),
-    emptyOutDir: false,
+    emptyOutDir: true,
   },
   plugins: [ViteEjsPlugin()],
   css: {

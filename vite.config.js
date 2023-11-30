870c7ecdac7801bae0090b26b0e2fc2caa01ffc6
mimu <akito.mimura@nijibox.co.jp>
2023-11-30 13:44:14 +0900

    add

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,29 @@ import { resolve } from "path";
 
 export default defineConfig({
   base: "/dist",
+  publicDir: "./src/images",
   build: {
     outDir: resolve(__dirname, "dist"),
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          console.log(assetInfo);
+          if (assetInfo.name.endsWith(".css")) {
+            return "assets/css/common.[extname]";
+          }
+          if (assetInfo.name.endsWith(".js")) {
+            return "assets/js/[name][extname]";
+          }
+          if (
+            /\.(gif|jpe?g|png|svg|tiff?|webp|bmp|ico)$/.test(assetInfo.name)
+          ) {
+            return "assets/images/[name][extname]";
+          }
+          return "assets/[name][extname]";
+        },
+      },
+    },
   },
   plugins: [ViteEjsPlugin()],
   css: {
